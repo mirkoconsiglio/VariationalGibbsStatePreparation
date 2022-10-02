c8014f589e19993e5a41fc93c0efdf73719d3486
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-10-02 15:18:26 +0100

    updated launcher

--- qiskit_runtime/vgsp_launcher.py
+++ qiskit_runtime/vgsp_launcher.py
@@ -1,14 +1,14 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
 service = QiskitRuntimeService()
-program_id = 'vgsp-ising-MN3nDB5Zv6'  # program id once it is uploaded
+program_id = 'vgsp-ising-XNvjgxPY4N'  # program id once it is uploaded
 backends = service.backends(simulator=False, min_num_qubits=4)
 backend_names = [backend.name for backend in backends]
-inputs = {"shots": 1024}  # Arguments passed to runtime. For POWELL: {"optimizer": POWELL()}
+inputs = {}
 
 for backend_name in backend_names:
 	try:
-		options = {"backend_name": backend_name}  # Choose backend
+		options = {"backend_name": backend_name}  # Choose backend (required)
 		# Run job
 		job = service.run(program_id, options=options, inputs=inputs)
 		print(f"Job sent to {backend_name} with job ID: {job.job_id}")
