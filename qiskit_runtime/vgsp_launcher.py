a731ef942fedb3df2a5c914b79a536a8eb6d402a
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-10-04 13:49:21 +0100

    will add streaming of results in the future

--- qiskit_runtime/vgsp_launcher.py
+++ qiskit_runtime/vgsp_launcher.py
@@ -1,16 +1,23 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
 service = QiskitRuntimeService()
-program_id = 'vgsp-ising-XNvjgxPY4N'  # program id once it is uploaded
+program_id = 'vgsp-ising-xVBvWpWqyV'  # program id once it is uploaded
 backends = service.backends(simulator=False, min_num_qubits=4)
-backend_names = [backend.name for backend in backends]
-inputs = {}
+inputs = dict(adiabatic_assistance=True)
 
-for backend_name in backend_names:
-	try:
-		options = {"backend_name": backend_name}  # Choose backend (required)
-		# Run job
-		job = service.run(program_id, options=options, inputs=inputs)
-		print(f"Job sent to {backend_name} with job ID: {job.job_id}")
-	except:
-		print(f"Job could not be sent to {backend_name}")
+backend_name = "ibmq_qasm_simulator"
+options = dict(backend_name=backend_name)  # Choose backend (required)
+# Run job
+job = service.run(program_id, options=options, inputs=inputs)
+print(f"Job sent to {backend_name} with job ID: {job.job_id}")
+
+# TODO: add streaming of results with callback function
+
+# for backend in backends:
+# 	try:
+# 		options = dict(backend_name=backend.name)  # Choose backend (required)
+# 		# Run job
+# 		job = service.run(program_id, options=options, inputs=inputs)
+# 		print(f"Job sent to {backend.name} with job ID: {job.job_id}")
+# 	except:
+# 		print(f"Job could not be sent to {backend.name}")
