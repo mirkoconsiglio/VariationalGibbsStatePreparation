1fe43d13e7c291125e625cbea4217a3c5e572d13
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-10-05 14:47:00 +0100

    added printing of results

--- qiskit_runtime/vgsp_launcher.py
+++ qiskit_runtime/vgsp_launcher.py
@@ -1,6 +1,9 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
+from job_results import print_results
 
+
+# noinspection PyUnusedLocal
 def callback(job_id, interim_result):
 	print(interim_result)
 
@@ -8,7 +11,6 @@ def callback(job_id, interim_result):
 if __name__ == '__main__':
 	service = QiskitRuntimeService()
 	program_id = 'vgsp-ising-xVBvWpWqyV'  # program id once it is uploaded
-	backends = service.backends(simulator=False, min_num_qubits=4)
 	inputs = dict()
 
 	backend_name = "ibmq_qasm_simulator"
@@ -16,10 +18,14 @@ if __name__ == '__main__':
 	# Run job
 	job = service.run(program_id, options=options, inputs=inputs)
 	print(f"Job sent to {backend_name} with job ID: {job.job_id}")
-
+	# Start streaming results
 	job.stream_results(callback)
+	# Get results
+	results = job.result()
+	# Print results
+	print_results(results)
 
-# for backend in backends:
+# for backend in service.backends(simulator=False, min_num_qubits=4):
 # 	try:
 # 		options = dict(backend_name=backend.name)  # Choose backend (required)
 # 		# Run job
