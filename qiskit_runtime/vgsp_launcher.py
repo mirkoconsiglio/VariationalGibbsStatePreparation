744aae7bbc54a7ae7652b3bc94e98a6fdc5edf79
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-11-15 15:52:06 +0000

    each beta can be split now due to runtime limits

--- qiskit_runtime/vgsp_launcher.py
+++ qiskit_runtime/vgsp_launcher.py
@@ -4,29 +4,41 @@ from gibbs_functions import print_multiple_results
 from plotter import plot_result_min_avg_max
 
 
-# noinspection PyUnusedLocal
-def callback(job_id, interim_result):
-	print(interim_result)
-
-
 def main():
-	n = 2
+	n = 5
 	J = 1.
 	h = 0.5
 	beta = [1e-10, 0.2, 0.5, 0.8, 1., 1.2, 2., 3., 4., 5.]
 	shots = 1024
 	N = 10
+	split_betas = True  # Split each beta into a separate job with N runs each
 
 	service = QiskitRuntimeService()
 	program_id = 'vgsp-ising-n6W7RRa7W6'  # program id once it is uploaded
-	inputs = dict(n=n, J=J, h=h, beta=beta, shots=shots, N=N)
-
 	backend_name = 'ibmq_qasm_simulator'
 	options = dict(backend_name=backend_name)  # Choose backend (required)
-	# Run job
-	job = service.run(program_id, options=options, inputs=inputs)
+
+	job = None
+	if not split_betas:
+		beta = [beta]
+	for b in beta:
+		# inputs
+		inputs = dict(n=n, J=J, h=h, beta=b, shots=shots, N=N)
+		# Run job
+		job = service.run(program_id, options=options, inputs=inputs)
+		job_id = job.job_id()
+		print(f"Job sent to {backend_name} with job ID: {job_id} for beta: {b}")
+	if not split_betas:
+		stream_results(job)
+
+
+def stream_results(job):
 	job_id = job.job_id()
-	print(f"Job sent to {backend_name} with job ID: {job_id}")
+	backend_name = job.backend.name
+	n = job.inputs.get('n')
+	J = job.inputs.get('J')
+	h = job.inputs.get('h')
+	shots = job.inputs.get('shots')
 	# Start streaming results
 	job.stream_results(callback)
 	# Get results
@@ -38,14 +50,9 @@ def main():
 	plot_result_min_avg_max(folder)
 
 
-# for backend in service.backends(simulator=False, min_num_qubits=4):
-# 	try:
-# 		options = dict(backend_name=backend.name)  # Choose backend (required)
-# 		# Run job
-# 		job = service.run(program_id, options=options, inputs=inputs)
-# 		print(f"Job sent to {backend.name} with job ID: {job.job_id}")
-# 	except:
-# 		print(f"Job could not be sent to {backend.name}")
+# noinspection PyUnusedLocal
+def callback(job_id, interim_result):
+	print(interim_result)
 
 
 if __name__ == '__main__':
