e36235e1a2ccbf5a48ab5124e1bb7595148851cd
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-11-16 16:08:17 +0000

    better testing locally

--- qiskit_runtime/test_qiskit_program.py
+++ qiskit_runtime/test_qiskit_program.py
@@ -2,10 +2,13 @@ from qiskit import IBMQ
 from qiskit_aer.noise import NoiseModel
 
 from gibbs_functions import print_multiple_results
+from plotter import plot_result_min_avg_max
 from vgsp_ising_program import main
 
 noise_model = NoiseModel.from_backend(IBMQ.load_account().get_backend('ibmq_jakarta')).to_dict()
 
-results = main(N=2, beta=[1., 2.])
+results = main(n=2, N=1, beta=[1.], noise_model=noise_model)
 
-print_multiple_results(results)
+print_multiple_results(results, output_folder='test_job')
+
+plot_result_min_avg_max('test_job')
