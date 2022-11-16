d9009d8dc98794236e3f9a0f8e484ef5a9a22e5b
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-11-16 16:07:24 +0000

    update to append to data

--- gibbs_functions.py
+++ gibbs_functions.py
@@ -69,20 +69,7 @@ def ising_hamiltonian(n: int, J: float = 1., h: float = 0.5) -> List[List[comple
 	return SummedOp(hamiltonian)
 
 
-def ising_hamiltonian_commuting_terms(n: int, J: float = 1., h: float = 0.5) -> dict:
-	terms = dict()
-	if J != 0:
-		if n == 2:
-			terms.update(x=[-J, [[0, 1]]])
-		elif n > 2:
-			terms.update(x=[-J, [[i, (i + 1) % n] for i in range(n)]])
-	if h != 0:
-		terms.update(z=[-h, list(range(n))])
-
-	return terms
-
-
-def _exact_result(hamiltonian, beta):
+def analytical_result(hamiltonian, beta):
 	hamiltonian_matrix = hamiltonian.to_matrix()
 	gibbs_state = exact_gibbs_state(hamiltonian_matrix, beta)
 	energy = (gibbs_state @ hamiltonian_matrix).diagonal().sum().real
@@ -101,14 +88,10 @@ def _exact_result(hamiltonian, beta):
 	)
 
 
-def _gibbs_result(gibbs):
+def gibbs_result(gibbs):
 	if isinstance(gibbs, dict):
 		return dict(
-			# ancilla_unitary_params=gibbs.ancilla_params(),
-			# system_unitary_params=gibbs.system_params(),
 			params=gibbs['params'],
-			# ancilla_unitary=gibbs.ancilla_unitary_matrix(),
-			# system_unitary=gibbs.system_unitary_matrix(),
 			cost=gibbs['cost'],
 			energy=gibbs['energy'],
 			entropy=gibbs['entropy'],
@@ -118,7 +101,6 @@ def _gibbs_result(gibbs):
 			noiseless_sigma=gibbs['noiseless_sigma'],
 			eigenvalues=gibbs['eigenvalues'],
 			noiseless_eigenvalues=gibbs['noiseless_eigenvalues'],
-			# eigenvectors=gibbs.eigenvectors,
 			hamiltonian_eigenvalues=gibbs['hamiltonian_eigenvalues'],
 			noiseless_hamiltonian_eigenvalues=gibbs['noiseless_hamiltonian_eigenvalues']
 		)
@@ -143,154 +125,58 @@ def _gibbs_result(gibbs):
 	)
 
 
-def print_results(results, output_folder=None):
-	if not isinstance(results, list):
-		results = [results]
-	if output_folder:
-		os.makedirs(f'{output_folder}', exist_ok=True)
-	for i, result in enumerate(results):
-		n = result.get('n')
-		J = result.get('J')
-		h = result.get('h')
-		beta = result['beta']
-		hamiltonian = ising_hamiltonian(n, J, h)
-		# get calculated results
-		calculated_result = _gibbs_result(result)
-		# Calculate exact results
-		exact_result = _exact_result(hamiltonian, beta)
-		# Calculate comparative results
-		ep = purity(exact_result['gibbs_state'])
-		cf = fidelity(exact_result['gibbs_state'], calculated_result['rho'])
-		ctd = trace_distance(exact_result['gibbs_state'], calculated_result['rho'])
-		cre = relative_entropy(exact_result['gibbs_state'], calculated_result['rho'])
-		cp = purity(calculated_result['rho'])
-		nf = fidelity(exact_result['gibbs_state'], calculated_result['noiseless_rho'])
-		ntd = trace_distance(exact_result['gibbs_state'], calculated_result['noiseless_rho'])
-		nre = relative_entropy(exact_result['gibbs_state'], calculated_result['noiseless_rho'])
-		np = purity(calculated_result['noiseless_rho'])
-		# Print results
-		print()
-		print(f"n: {n}")
-		print(f"J: {J}")
-		print(f"h: {h}")
-		print(f"beta: {beta}")
-		print()
-		print("Exact Gibbs state: ")
-		print(exact_result['gibbs_state'])
-		print()
-		print("Calculated Gibbs state: ")
-		print(calculated_result['rho'])
-		print()
-		print("Noiseless Calculated Gibbs state: ")
-		print(calculated_result['noiseless_rho'])
-		print()
-		print(f"VQA cost: {calculated_result['cost']}")
-		print(f"Exact cost: {exact_result['cost']}")
-		print()
-		print(f"VQA energy: {calculated_result['energy']}")
-		print(f"Exact energy: {exact_result['energy']}")
-		print()
-		print(f"VQA entropy: {calculated_result['entropy']}")
-		print(f"Exact entropy: {exact_result['entropy']}")
-		print()
-		print(f"VQA eigenvalues: {calculated_result['eigenvalues']}")
-		print(f"Exact eigenvalues: {exact_result['eigenvalues']}")
-		print()
-		print(f"VQA Hamiltonian eigenvalues: {calculated_result['hamiltonian_eigenvalues']}")
-		print(f"Exact Hamiltonian eigenvalues: {exact_result['hamiltonian_eigenvalues']}")
-		print()
-		print(f"Exact Purity: {ep}")
-		print()
-		print(f"Calculated Fidelity: {cf}")
-		print(f"Calculated Trace Distance: {ctd}")
-		print(f"Calculated Relative Entropy: {cre}")
-		print(f"Calculated Purity: {cp}")
-		print()
-		print(f"Noiseless Fidelity: {nf}")
-		print(f"Noiseless Trace Distance: {ntd}")
-		print(f"Noiseless Relative Entropy: {nre}")
-		print(f"Noiseless Purity: {np}")
-		print()
-
-		if output_folder:
-			data = dict(
-				metadata=dict(
-					n=n,
-					J=J,
-					h=h,
-					beta=beta,
-					ancilla_reps=result.get('ancilla_reps'),
-					system_reps=result.get('system_reps'),
-					skip_transpilation=result.get('skip_transpilation'),
-					use_measurement_mitigation=result.get('use_measurement_mitigation'),
-					ansatz=result.get('ansatz'),
-					optimizer=result.get('optimizer'),
-					min_kwargs=result.get('min_kwargs'),
-					shots=result.get('shots'),
-					backend=result.get('backend')
-				),
-				calculated_result=calculated_result,
-				exact_result=exact_result,
-				metrics=dict(
-					exact_purity=ep,
-					calculated_fidelity=cf,
-					calculated_trace_distance=ctd,
-					calculated_relative_entropy=cre,
-					calculated_purity=cp,
-					noiseless_fidelity=nf,
-					noiseless_trace_distance=ntd,
-					noiseless_relative_entropy=nre,
-					noiseless_purity=np,
-				)
-			)
-
-			with open(f'{output_folder}/{beta:.2f}.json', 'w') as f:
-				json.dump(data, f, indent=4, cls=ResultsEncoder)
-
-
-def print_multiple_results(multiple_results, output_folder=None, job_id=None, backend=None):
+def print_multiple_results(multiple_results, output_folder=None, job_id=None, backend=None, append=True):
 	if not isinstance(multiple_results, list):
 		multiple_results = [multiple_results]
 	if output_folder:
 		os.makedirs(f'{output_folder}', exist_ok=True)
-	for results in multiple_results:
-		n = None
-		J = None
-		h = None
-		beta = None
-		hamiltonian = None
-		ancilla_reps = None
-		system_reps = None
-		optimizer = None
-		min_kwargs = None
-		shots = None
-		ep = None
-		cf_list = []
-		ctd_list = []
-		cre_list = []
-		cp_list = []
-		nf_list = []
-		ntd_list = []
-		nre_list = []
-		np_list = []
-		for i, result in enumerate(results):
-			if i == 0:
-				n = result.get('n')
-				J = result.get('J')
-				h = result.get('h')
-				beta = result.get('beta')
-				hamiltonian = ising_hamiltonian(n, J, h)
-				ancilla_reps = result.get('ancilla_reps')
-				optimizer = result.get('optimizer')
-				min_kwargs = result.get('min_kwargs')
-				shots = result.get('shots')
+	for results in multiple_results:  # Different beta
+		# Calculate exact results
+		# Assumes these will be the same for each different job
+		result = results[0]
+		n = result.get('n')
+		J = result.get('J')
+		h = result.get('h')
+		beta = result.get('beta')
+		hamiltonian = ising_hamiltonian(n, J, h)
+		ancilla_reps = result.get('ancilla_reps')
+		system_reps = result.get('system_reps')
+		optimizer = result.get('optimizer')
+		min_kwargs = result.get('min_kwargs')
+		shots = result.get('shots')
+		exact_result = analytical_result(hamiltonian, beta)
+		ep = purity(exact_result['gibbs_state'])
+		# load data if append is True
+		data = dict()
+		if append:
+			try:
+				with open(f'{output_folder}/{beta:.2f}.json', 'r') as f:
+					data = json.load(f)
+			except FileNotFoundError:
+				pass
+		# Keep job ids and backends
+		metadata = data.get('metadata', dict())
+		job_ids = metadata.get('job_ids', [])
+		backends = metadata.get('backends', [])
+		job_ids.append(job_id)
+		backends.append(backend)
+		job_ids = list(set(job_ids))
+		backends = list(set(backends))
+		# Set up metrics
+		metrics = data.get('metrics', dict())
+		cf_list = metrics.get('calculated_fidelity', [])
+		ctd_list = metrics.get('calculated_trace_distance', [])
+		cre_list = metrics.get('calculated_relative_entropy', [])
+		cp_list = metrics.get('calculated_purity', [])
+		nf_list = metrics.get('noiseless_fidelity', [])
+		ntd_list = metrics.get('noiseless_trace_distance', [])
+		nre_list = metrics.get('noiseless_relative_entropy', [])
+		np_list = metrics.get('noiseless_purity', [])
+		# Calculate and save data
+		for result in results:  # Different runs
 			# get calculated results
-			calculated_result = _gibbs_result(result)
-			# Calculate exact results
-			exact_result = _exact_result(hamiltonian, beta)
+			calculated_result = gibbs_result(result)
 			# Calculate comparative results
-			if i == 0:
-				ep = purity(exact_result['gibbs_state'])
 			cf_list.append(fidelity(exact_result['gibbs_state'], calculated_result['rho']))
 			ctd_list.append(trace_distance(exact_result['gibbs_state'], calculated_result['rho']))
 			cre_list.append(relative_entropy(exact_result['gibbs_state'], calculated_result['rho']))
@@ -338,8 +224,8 @@ def print_multiple_results(multiple_results, output_folder=None, job_id=None, ba
 		if output_folder:
 			data = dict(
 				metadata=dict(
-					job_id=job_id,
-					backend=backend,
+					job_ids=job_ids,
+					backends=backends,
 					n=n,
 					J=J,
 					h=h,
