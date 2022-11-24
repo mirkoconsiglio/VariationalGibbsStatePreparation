86cf1f516822801ade24fa75cad7492c22a3f40f
Mirko Consiglio <mirkoconsiglio@hotmail.co.uk>
2022-11-24 16:11:46 +0000

    can run from console with arguments

--- qiskit_runtime/test_qiskit_program.py
+++ qiskit_runtime/test_qiskit_program.py
@@ -1,13 +1,21 @@
+from argparse import ArgumentParser
+
 from gibbs_functions import print_multiple_results
 from plotter import plot_result_min_avg_max
 from vgsp_ising_program import main
 
-noise_model = 'ibmq_jakarta'
-
-results = main(n=2, N=1, beta=[1.], noise_model=noise_model)
-
-folder = 'test_job'
-
-print_multiple_results(results, output_folder=folder)
-
-plot_result_min_avg_max(folder)
+if __name__ == '__main__':
+	parser = ArgumentParser(description='VGSP')
+	parser.add_argument('--n', default=2, type=int)
+	parser.add_argument('--N', default=1, type=int)
+	parser.add_argument('--beta', default=1., type=float, nargs='+')
+	parser.add_argument('--noise_model', default=None, type=str)
+	args = vars(parser.parse_args())
+	
+	results = main(**args)
+	
+	folder = 'test_job'
+	
+	print_multiple_results(results, output_folder=folder)
+	
+	plot_result_min_avg_max(folder, show=False)
