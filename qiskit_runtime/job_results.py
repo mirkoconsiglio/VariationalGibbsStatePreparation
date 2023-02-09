83010b0a6c8888486881cc56f9fc03f2723f355a
Mirko Consiglio <mirko.consiglio@um.edu.mt>
2023-02-09 11:59:09 +0100

    adjusted job results script since API update in Qiskit Runtime

--- qiskit_runtime/job_results.py
+++ qiskit_runtime/job_results.py
@@ -4,9 +4,9 @@ from json import JSONDecodeError
 from qiskit_ibm_runtime import QiskitRuntimeService, RuntimeDecoder, RuntimeJobFailureError, RuntimeJobMaxTimeoutError
 
 from gibbs_functions import print_multiple_results
-from plotter import plot_result_min_avg_max
 
 
+# Deprecated
 def decode_interim_results(data, N=1):
 	results = []
 	for i in reversed(data):
@@ -25,38 +25,37 @@ def decode_interim_results(data, N=1):
 
 
 def main():
-	service = QiskitRuntimeService()
-	# jobs = service.jobs(limit=10, skip=20)
-	jobs = [service.job('cei3i95iuboftdmpnc70')]
+	service = QiskitRuntimeService(name='personal')
+	jobs = service.jobs(limit=10, skip=7)
 	append = True  # Append results or overwrite
-	folders = set()
 	for job in jobs:
 		job_id = job.job_id()
-		backend_name = job.backend.name
-		n = job.inputs.get('n')
-		J = job.inputs.get('J')
-		h = job.inputs.get('h')
-		shots = job.inputs.get('shots')
-		noise_model = job.inputs.get('noise_model')
-
 		# Get job results
 		try:
 			results = job.result()
-		except (RuntimeJobFailureError, RuntimeJobMaxTimeoutError) as err:
-			print(err.message)
-			print("Since final results could not be retrieved, getting interim results instead.")
-			results = decode_interim_results(job.interim_results(), job.inputs.get('N'))
+		except RuntimeJobMaxTimeoutError:
+			print(f"Runtime job {job_id} timed out.")
+			continue
+		except RuntimeJobFailureError:
+			print(f"Runtime job {job_id} failed.")
+			continue
+		else:
+			print(f"Runtime job {job_id} succeeded.")
+
+		# These will be the same for every batched run in the job
+		backend_name = results[0][0].get('backend')
+		n = results[0][0].get('n')
+		J = results[0][0].get('J')
+		h = results[0][0].get('h')
+		shots = results[0][0].get('shots')
+		noise_model = results[0][0].get('noise_model')
 
 		folder = f'jobs/{backend_name}'
 		if isinstance(noise_model, str):
 			folder += f'_{noise_model}'
 		folder += f'/n_{n}_J_{J:.2f}_h_{h:.2f}_shots_{shots}'
-		folders.add(folder)
 
 		print_multiple_results(results, output_folder=folder, job_id=job_id, backend=backend_name, append=append)
-
-	for folder in folders:
-		plot_result_min_avg_max(folder)
 
 
 if __name__ == '__main__':
