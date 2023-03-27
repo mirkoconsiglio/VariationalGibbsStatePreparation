f8546b8bbd3fb97c9f2cb49cdfb999af618e126c
Mirko Consiglio <mirko.consiglio@um.edu.mt>
2023-03-27 13:26:04 +0200

    updated test script

--- qiskit_runtime/test_qiskit_program.py
+++ qiskit_runtime/test_qiskit_program.py
@@ -1,21 +1,18 @@
 from argparse import ArgumentParser
 
 from gibbs_functions import print_multiple_results
-from plotter import plot_result_min_avg_max
 from vgsp_ising_program import main
 
 if __name__ == '__main__':
 	parser = ArgumentParser(description='VGSP')
-	parser.add_argument('--n', default=2, type=int)
+	parser.add_argument('--n', default=3, type=int)
 	parser.add_argument('--N', default=1, type=int)
-	parser.add_argument('--beta', default=1., type=float, nargs='+')
-	parser.add_argument('--noise_model', default='ibmq_montreal', type=str)
+	parser.add_argument('--beta', default=0.5, type=float, nargs='+')
+	parser.add_argument('--noise_model', default='ibmq_guadalupe', type=str)
 	parser.add_argument('--folder', default='test_job', type=str)
 	args = vars(parser.parse_args())
 	folder = args.pop('folder')
 
 	results = main(**args)
 
-	print_multiple_results(results, output_folder=folder)
-
-	plot_result_min_avg_max(folder, show=False)
+	print_multiple_results(results, output_folder=folder, zip_file=False)
