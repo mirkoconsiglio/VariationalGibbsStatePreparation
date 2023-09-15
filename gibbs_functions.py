a375e298b5458c32fe40afeb2bb003a42cfe0bb3
Mirko Consiglio <mirko.consiglio@um.edu.mt>
2023-09-15 15:17:39 +0200

    updated gibbs_functions.py

--- gibbs_functions.py
+++ gibbs_functions.py
@@ -5,9 +5,7 @@ import warnings
 
 import numpy as np
 from numpy.linalg import eigh
-from qiskit.opflow import SummedOp
-from qiskit.opflow.primitive_ops import PauliOp
-from qiskit.quantum_info import Pauli
+from qiskit.quantum_info.operators import SparsePauliOp
 from qiskit_ibm_runtime import RuntimeEncoder
 from scipy.linalg import norm, logm, expm
 from scipy.stats import entropy
@@ -53,22 +51,32 @@ def exact_gibbs_state(hamiltonian, beta):
 	return rho / rho.diagonal().sum()
 
 
-def ising_hamiltonian(n, J=1., h=0.5):
+def XXZ_hamiltonian(n, gamma=1., delta=1., h=0.):
 	hamiltonian = []
+	J_x = 1 + gamma
+	J_y = 1 - gamma
 	for i in range(n):
 		# Interaction terms
 		if i != n - 1:
-			XX = 'I' * i + 'XX' + 'I' * (n - i - 2)
-			hamiltonian.append(PauliOp(Pauli(XX), -J))
+			if J_x != 0:
+				hamiltonian.append(('I' * i + 'XX' + 'I' * (n - i - 2), -0.25 * J_x))
+			if J_y != 0:
+				hamiltonian.append(('I' * i + 'YY' + 'I' * (n - i - 2), -0.25 * J_y))
+			if delta != 0:
+				hamiltonian.append(('I' * i + 'ZZ' + 'I' * (n - i - 2), -0.25 * delta))
 		elif n > 2:
-			XX = 'X' + 'I' * (n - 2) + 'X'
-			hamiltonian.append(PauliOp(Pauli(XX), -J))
+			if J_x != 0:
+				hamiltonian.append(('X' + 'I' * (n - 2) + 'X', -0.25 * J_x))
+			if J_y != 0:
+				hamiltonian.append(('Y' + 'I' * (n - 2) + 'Y', -0.25 * J_y))
+			if delta != 0:
+				hamiltonian.append(('Z' + 'I' * (n - 2) + 'Z', -0.25 * delta))
 		# Magnetic terms
-		Z = 'I' * i + 'Z' + 'I' * (n - i - 1)
-		hamiltonian.append(PauliOp(Pauli(Z), -h))
+		if h != 0:
+			hamiltonian.append(('I' * i + 'Z' + 'I' * (n - i - 1), -0.25 * h))
 
 	# Build up the Hamiltonian
-	return SummedOp(hamiltonian)
+	return SparsePauliOp.from_list(hamiltonian)
 
 
 def analytical_result(hamiltonian, beta):
@@ -76,7 +84,7 @@ def analytical_result(hamiltonian, beta):
 	gibbs_state = exact_gibbs_state(hamiltonian_matrix, beta)
 	energy = (gibbs_state @ hamiltonian_matrix).diagonal().sum().real
 	vn_entropy = von_neumann_entropy(gibbs_state)
-	cost = energy - vn_entropy / beta
+	cost = beta * energy - vn_entropy
 	eigenvalues = np.linalg.eigh(gibbs_state)[0]
 	hamiltonian_eigenvalues = np.linalg.eigh(hamiltonian_matrix)[0]
 	return dict(
@@ -127,7 +135,109 @@ def gibbs_result(gibbs):
 	)
 
 
-def print_multiple_results(multiple_results, output_folder=None, job_id=None, backend=None, append=True, zip_file=True):
+def save_result(result, output_folder, job_id=None, backend=None, append=True):
+	if output_folder:
+		os.makedirs(output_folder, exist_ok=True)
+	# Get results
+	n = result.get('n')
+	gamma = result.get('gamma')
+	delta = result.get('delta')
+	h = result.get('h')
+	beta = result.get('beta')
+	hamiltonian = XXZ_hamiltonian(n, gamma, delta, h)
+	ancilla_reps = result.get('ancilla_reps')
+	system_reps = result.get('system_reps')
+	optimizer = result.get('optimizer')
+	min_kwargs = result.get('min_kwargs')
+	shots = result.get('shots')
+	noise_model = result.get('noise_model_backend')
+	exact_result = analytical_result(hamiltonian, beta)
+	ep = purity(exact_result['gibbs_state'])
+	# load data if append is True
+	data = dict()
+	if append:
+		try:
+			with open(f'{output_folder}/{beta:.2f}.json', 'r') as f:
+				data = json.load(f)
+		except FileNotFoundError:
+			pass
+	# Keep job ids and backends
+	metadata = data.get('metadata', dict())
+	job_ids = metadata.get('job_ids', [])
+	backends = metadata.get('backends', [])
+	job_ids.append(job_id)
+	backends.append(backend)
+	# Do not add backends or job ids if they are already added
+	job_ids = list(set(job_ids))
+	backends = list(set(backends))
+	# Set up metrics
+	metrics = data.get('metrics', dict())
+	cf_list = metrics.get('calculated_fidelity', [])
+	ctd_list = metrics.get('calculated_trace_distance', [])
+	cre_list = metrics.get('calculated_relative_entropy', [])
+	cp_list = metrics.get('calculated_purity', [])
+	ckld_list = metrics.get('calculated_kullback_leibler_divergence', [])
+	nf_list = metrics.get('noiseless_fidelity', [])
+	ntd_list = metrics.get('noiseless_trace_distance', [])
+	nre_list = metrics.get('noiseless_relative_entropy', [])
+	np_list = metrics.get('noiseless_purity', [])
+	nkld_list = metrics.get('noiseless_kullback_leibler_divergence', [])
+	# Calculate and save data
+	calculated_results = data.get('calculated_result', [])
+	# get calculated results
+	calculated_result = gibbs_result(result)
+	calculated_results.append(calculated_result)
+	# Calculate comparative results
+	cf_list.append(fidelity(exact_result['gibbs_state'], calculated_result['rho']))
+	ctd_list.append(trace_distance(exact_result['gibbs_state'], calculated_result['rho']))
+	cre_list.append(relative_entropy(exact_result['gibbs_state'], calculated_result['rho']))
+	cp_list.append(purity(calculated_result['rho']))
+	ckld_list.append(entropy(exact_result['eigenvalues'], calculated_result['eigenvalues']))
+	nf_list.append(fidelity(exact_result['gibbs_state'], calculated_result['noiseless_rho']))
+	ntd_list.append(trace_distance(exact_result['gibbs_state'], calculated_result['noiseless_rho']))
+	nre_list.append(relative_entropy(exact_result['gibbs_state'], calculated_result['noiseless_rho']))
+	np_list.append(purity(calculated_result['noiseless_rho']))
+	nkld_list.append(entropy(exact_result['eigenvalues'], calculated_result['noiseless_eigenvalues']))
+	# Set up data dictionary
+	data = dict(
+		metrics=dict(
+			exact_purity=ep,
+			calculated_fidelity=cf_list,
+			calculated_trace_distance=ctd_list,
+			calculated_relative_entropy=cre_list,
+			calculated_purity=cp_list,
+			calculated_kullback_leibler_divergence=ckld_list,
+			noiseless_fidelity=nf_list,
+			noiseless_trace_distance=ntd_list,
+			noiseless_relative_entropy=nre_list,
+			noiseless_purity=np_list,
+			noiseless_kullback_leibler_divergence=nkld_list,
+		),
+		metadata=dict(
+			job_ids=job_ids,
+			backends=backends,
+			n=n,
+			gamma=gamma,
+			delta=delta,
+			h=h,
+			beta=beta,
+			ancilla_reps=ancilla_reps,
+			system_reps=system_reps,
+			optimizer=optimizer,
+			min_kwargs=min_kwargs,
+			shots=shots,
+			noise_model=noise_model,
+		),
+		calculated_result=calculated_results,
+		exact_result=exact_result
+	)
+
+	with open(f'{output_folder}/{beta:.2f}.json', 'w') as f:
+		json.dump(data, f, indent=4, cls=ResultsEncoder)
+
+
+def print_multiple_results(multiple_results, output_folder=None, job_id=None, backend=None, append=True, zip_file=True,
+                           hamiltonian='XXZ'):
 	if output_folder:
 		os.makedirs(output_folder, exist_ok=True)
 	# Check if there is already data saved to add to it rather than overwrite
@@ -146,10 +256,17 @@ def print_multiple_results(multiple_results, output_folder=None, job_id=None, ba
 		# Assumes these will be the same for each job
 		result = results[0]
 		n = result.get('n')
-		J = result.get('J')
+		if hamiltonian == 'Ising':
+			gamma = 1
+		else:
+			gamma = result.get('gamma')
+		if hamiltonian != 'XXZ':
+			delta = 0
+		else:
+			delta = result.get('delta')
 		h = result.get('h')
 		beta = result.get('beta')
-		hamiltonian = ising_hamiltonian(n, J, h)
+		hamiltonian = XXZ_hamiltonian(n, gamma, delta, h)
 		ancilla_reps = result.get('ancilla_reps')
 		system_reps = result.get('system_reps')
 		optimizer = result.get('optimizer')
@@ -193,6 +310,10 @@ def print_multiple_results(multiple_results, output_folder=None, job_id=None, ba
 			# get calculated results
 			calculated_result = gibbs_result(result)
 			calculated_results.append(calculated_result)
+			print()
+			print(calculated_result)
+			print(exact_result)
+			print()
 			# Calculate comparative results
 			cf_list.append(fidelity(exact_result['gibbs_state'], calculated_result['rho']))
 			ctd_list.append(trace_distance(exact_result['gibbs_state'], calculated_result['rho']))
@@ -207,7 +328,8 @@ def print_multiple_results(multiple_results, output_folder=None, job_id=None, ba
 		# Print results
 		print()
 		print(f"n: {n}")
-		print(f"J: {J}")
+		print(f"gamma: {gamma}")
+		print(f"delta: {delta}")
 		print(f"h: {h}")
 		print(f"beta: {beta}")
 		print()
@@ -265,7 +387,8 @@ def print_multiple_results(multiple_results, output_folder=None, job_id=None, ba
 					job_ids=job_ids,
 					backends=backends,
 					n=n,
-					J=J,
+					gamma=gamma,
+					delta=delta,
 					h=h,
 					beta=beta,
 					ancilla_reps=ancilla_reps,
